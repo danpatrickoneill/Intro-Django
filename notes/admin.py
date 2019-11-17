f6746e1c915718b96931209d020df5a02319fe76
danpatrickoneill <dan47573@gmail.com>
2019-11-17 15:11:19 -0500

    Day 2 complete

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Note, NoteAdmin, PersonalNote
+
 
 # Register your models here.
+admin.site.register(Note, NoteAdmin)
+admin.site.register(PersonalNote)
--- notes/models.py
+++ notes/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib import admin
+from django.contrib.auth.models import User
 from uuid import uuid4
 
 # Create your models here.
@@ -9,3 +11,13 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
     author = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+
+class NoteAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'last_modified')
+
+
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
